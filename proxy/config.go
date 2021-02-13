3aeac497e40ce6163f920ab48856e832d16b38f5
s1ntaxe770r <cyberdev01@protonmail.com>
2021-02-13 05:07:55 -0800

    load toml file

--- proxy/config.go
+++ proxy/config.go
@@ -1,8 +1,18 @@
 package proxy
 
+import "github.com/pelletier/go-toml"
+
 // struct representation of toml config
 type ProxyConfig struct {
 	path        string
 	destination int
 	entrypoint  int
+}
+ 
+func LoadConfig() *ProxyConfig {
+	config, err := go-toml.Load("config.toml")
+	if err != nil {
+		fmt.Println("could not load toml config")
+	}
+
 }
