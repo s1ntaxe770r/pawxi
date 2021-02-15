faa79e48d750e1317d1174fd57c53a55ffede78f
s1ntaxe770r <cyberdev01@protonmail.com>
2021-02-15 11:19:55 -0800

    refactor proxy package

--- proxy/config.go
+++ proxy/config.go
@@ -6,19 +6,24 @@ import (
 	"github.com/pelletier/go-toml"
 )
 
-// struct representation of toml config
-type ProxyConfig struct {
+// Config : struct representation of a toml config
+type Config struct {
 	path        string
 	destination int
 	entrypoint  int
 }
 
-func LoadConfig() *ProxyConfig {
+// LoadConfig  reads a toml file and returns a Config struct
+func LoadConfig() *Config {
 	config, err := toml.LoadFile("config.toml")
 	if err != nil {
 		fmt.Println("could not load toml config")
 	}
 
-	proxyconf := ProxyConfig{path: config.Get("path").(string), destination: config.Get("destination").(string)}
-
+	proxyconf := Config{
+		path:        config.Get("path").(string),
+		destination: config.Get("destination").(int),
+		entrypoint:  config.Get("entrypoint").(int),
+	}
+	return &proxyconf
 }
