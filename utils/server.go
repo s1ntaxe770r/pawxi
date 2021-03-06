096137829f7f00cc4a765c0394609aa44139c637
s1ntaxe770r <cyberdev01@protonmail.com>
2021-03-06 12:41:15 +0100

    add bind address argument

--- utils/server.go
+++ utils/server.go
@@ -5,13 +5,15 @@ import (
 	"time"
 )
 
-func NewServer(servemux *http.ServeMux) *http.Server {
+// NewServer returns a new http server with custom timeouts
+func NewServer(servemux *http.ServeMux, port string) *http.Server {
 	// enforce proper timeouts on servver
 	server := &http.Server{
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 5 * time.Second,
 		IdleTimeout:  120 * time.Second,
 		Handler:      servemux,
+		Addr:         port,
 	}
 
 	return server
