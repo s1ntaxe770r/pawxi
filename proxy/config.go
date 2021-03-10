7ed9767b59d995be91e6cc6da7ce894e90074255
s1ntaxe770r <cyberdev01@protonmail.com>
2021-03-10 16:40:06 +0100

    update config to support maps

--- proxy/config.go
+++ proxy/config.go
@@ -1,43 +1,17 @@
 package proxy
 
-import (
-	"log"
-
-	"github.com/pelletier/go-toml"
-)
-
 // Config : struct representation of a toml config
 type Config struct {
-	Domain      string
-	Path        string
-	Destination string
-	Entrypoint  string
-	UseGzip     string
+	Domain  string
+	UseGzip string
 }
+
+// Route maps a path to a backend or destination. Eg: /home => http:localhost:6000/
+type Route struct {
+	Path, Destination string
+}
+
+// ProxyRoutes a slice of Route
+var ProxyRoutes []Route
 
 // NewConfig  Returns a pointer to a new config struct
-func NewConfig() *Config {
-	return &Config{}
-}
-
-// LoadConfig reads pawxi.toml and returns a struct of type Config
-func LoadConfig() *Config {
-	config, err := toml.LoadFile("pawxi.toml")
-	if err != nil {
-		log.Panicf("could not load toml file %s", err.Error())
-	}
-	path := config.Get("proxy.path").(string)
-	destination := config.Get("proxy.destination").(string)
-	entrypoint := config.Get("proxy.entrypoint").(string)
-	usegzip := config.Get("proxy.entrypoint").(string)
-	// todo: validate data
-
-	parsedtoml := Config{
-		Path:        path,
-		Destination: destination,
-		Entrypoint:  entrypoint,
-		UseGzip:     usegzip,
-	}
-	return &parsedtoml
-
-}
