2a8500fcec3ef05360166bd94a1719230b3b6b2f
s1ntaxe770r <cyberdev01@protonmail.com>
2021-03-10 18:29:27 +0100

    re implement gzip

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/url"
 
+	"github.com/NYTimes/gziphandler"
 	"github.com/s1ntaxe770r/pawxi/proxy"
 	"github.com/s1ntaxe770r/pawxi/utils"
 
@@ -18,23 +19,28 @@ func handle(err error) {
 	}
 }
 
+var (
+	r      = http.NewServeMux()
+	routes []proxy.Route
+)
+
 func main() {
-	r := http.NewServeMux()
-	server := utils.NewServer(r, ":80")
-	var routes []proxy.Route
+
 	viper.AddConfigPath(".")
 	viper.SetConfigType("toml")
 	viper.SetConfigName("pawxi")
 	readerr := viper.ReadInConfig()
-	if readerr != nil {
-		panic(fmt.Errorf("fatal error config file: %s \n", readerr))
-	}
+	port := fmt.Sprintf(":%s", viper.GetString("proxy.binds"))
+	server := utils.NewServer(r, port)
 
+	if readerr != nil {
+		panic(fmt.Errorf("fatal error config file: %s", readerr))
+	}
 	err := viper.UnmarshalKey("proxy.routes", &routes)
 	if err != nil {
 		log.Fatalf("unable to unmarshal into strcut REASON : %v", err.Error())
 	}
-
+	usegzip := viper.GetBool("proxy.usegzip")
 	for _, route := range routes {
 		// parse each url
 		dest, err := url.Parse(route.Destination)
@@ -43,12 +49,15 @@ func main() {
 		}
 		// indiviually proxy each request
 		proxy := proxy.Tunnel(route, dest)
-		r.HandleFunc(route.Path, func(w http.ResponseWriter, r *http.Request) {
-			proxy.ServeHTTP(w, r)
-		})
+		if usegzip != true {
+			r.HandleFunc(route.Path, func(w http.ResponseWriter, r *http.Request) {
+				proxy.ServeHTTP(w, r)
+			})
+		} else {
+			zipped := gziphandler.GzipHandler(proxy)
+			r.Handle(route.Path, zipped)
+		}
 	}
-
-	log.Println("PROXYING ON PORT 80")
 	server.ListenAndServe()
 
 }
