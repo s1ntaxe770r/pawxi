ba3ebfc96a9ed1139482a184ea32617d3e6b30fb
s1ntaxe770r <cyberdev01@protonmail.com>
2021-03-13 18:06:37 +0100

    use logrus and prettyfiy output

--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"net/url"
-	"path/filepath"
+	"time"
 
 	"github.com/NYTimes/gziphandler"
+	"github.com/briandowns/spinner"
 	"github.com/fsnotify/fsnotify"
+	"github.com/gorilla/mux"
 	"github.com/s1ntaxe770r/pawxi/proxy"
-	"github.com/s1ntaxe770r/pawxi/tls"
 	"github.com/s1ntaxe770r/pawxi/utils"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
@@ -22,7 +23,7 @@ func handle(err error) {
 }
 
 var (
-	r      = http.NewServeMux()
+	router = mux.NewRouter()
 	routes []proxy.Route
 )
 
@@ -33,46 +34,43 @@ func main() {
 	viper.SetConfigName("pawxi")
 	readerr := viper.ReadInConfig()
 	port := fmt.Sprintf(":%s", viper.GetString("proxy.binds"))
-	server := utils.NewServer(r, port)
+	server := utils.NewServer(router, port)
 	viper.WatchConfig()
 	viper.OnConfigChange(func(e fsnotify.Event) {
-		fmt.Println("Config file changed:", e.Name)
-
+		logrus.Warn("Config file changed:", e.Name)
+		logrus.Warn("Restarting Proxy")
+		s := spinner.New(spinner.CharSets[2], 100*time.Millisecond)
+		s.Start()
+		s.Color("yellow")
+		// Semi Graceful shutdown ?
+		time.Sleep(5 * time.Second)
+		s.Stop()
+		utils.Exec()
 	})
 	if readerr != nil {
 		panic(fmt.Errorf("fatal error config file: %s", readerr))
 	}
 	err := viper.UnmarshalKey("proxy.routes", &routes)
 	if err != nil {
-		log.Fatalf("unable to unmarshal into strcut REASON : %v", err.Error())
+		logrus.Error("unable to unmarshal into strcut REASON : %v", err.Error())
 	}
 	usegzip := viper.GetBool("proxy.usegzip")
 	for _, route := range routes {
-		// parse each url
-		dest, err := url.Parse(route.Destination)
-		if err != nil {
-			log.Fatalf("could not parse url %s", route.Destination)
-		}
 		// indiviually proxy each request
-		proxy := proxy.Tunnel(route, dest)
+		proxy := proxy.Tunnel(route)
 		if usegzip != true {
-			go r.HandleFunc(route.Path, func(w http.ResponseWriter, r *http.Request) {
+			router.HandleFunc(route.Path, func(w http.ResponseWriter, r *http.Request) {
 				proxy.ServeHTTP(w, r)
+
 			})
 		} else {
 			zipped := gziphandler.GzipHandler(proxy)
-			go r.Handle(route.Path, zipped)
+			router.Handle(route.Path, zipped)
 		}
 	}
-	tls.SetupDevCerts()
-	vendorPath := "./certs"
-	cert := filepath.Join(vendorPath, "devcerts", "cert.pem")
-	key := filepath.Join(vendorPath, "devcerts", "key.pem")
-	go func() {
-		if err := http.ListenAndServe(port, http.HandlerFunc(utils.RedirectTLS)); err != nil {
-			log.Fatalf("ListenAndServe error: %v", err)
-		}
-	}()
-	server.ListenAndServeTLS(cert, key)
+	logrus.Info("proxy started on ", port)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("ListenAndServe error: %v", err)
+	}
 
 }
