82c0b465a8264e483148f9303d194ff7fe7b72aa
s1ntaxe770r <cyberdev01@protonmail.com>
2021-03-13 18:44:25 +0100

    CONFIG IS NOW VIZUALIZED

--- utils/routes_viz.go
+++ utils/routes_viz.go
@@ -1,1 +1,20 @@
 package utils
+
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/s1ntaxe770r/pawxi/proxy"
+)
+
+func Vizualize(routes []proxy.Route) {
+	t := table.NewWriter()
+	tTemp := table.Table{}
+	tTemp.Render()
+	for _, route := range routes {
+		t.AppendRow([]interface{}{color.YellowString(route.Path), color.MagentaString("=>"), color.GreenString(route.Destination)})
+	}
+	t.SetCaption("current routes setup.\n")
+	fmt.Println(t.Render())
+}
