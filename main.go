00385a26dbad2eaecb8859a142bc768b8203b8bb
Jubril Oyetunji <cyberdev01@protonmail.com>
2021-06-22 13:38:44 +0100

    panic if port is not set

--- main.go
+++ main.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"time"
-
 	"github.com/NYTimes/gziphandler"
 	"github.com/briandowns/spinner"
 	"github.com/fatih/color"
 	"github.com/fsnotify/fsnotify"
 	"github.com/gorilla/mux"
-	"github.com/s1ntaxe770r/pawxi/metrics"
 	"github.com/s1ntaxe770r/pawxi/proxy"
 	"github.com/s1ntaxe770r/pawxi/utils"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+	"log"
+	"net/http"
+	"time"
 )
 
 func handle(err error) {
@@ -29,15 +27,16 @@ var (
 	routes []proxy.Route
 )
 
-func main() {
 
+
+func main() {
 	viper.AddConfigPath(".")
 	viper.SetConfigType("toml")
 	viper.SetConfigName("pawxi")
 	readerr := viper.ReadInConfig()
 	port := fmt.Sprintf(":%s", viper.GetString("proxy.binds"))
 	server := utils.NewServer(router, port)
-	router.Use(metrics.Count_Request)
+
 	viper.WatchConfig()
 	viper.OnConfigChange(func(e fsnotify.Event) {
 		logrus.Warn("Config file changed:", e.Name)
@@ -55,11 +54,11 @@ func main() {
 	}
 	err := viper.UnmarshalKey("proxy.routes", &routes)
 	if err != nil {
-		logrus.Error("unable to unmarshal into strcut REASON : %v", err.Error())
+		logrus.Error("unable to unmarshal into struct REASON : %v", err.Error())
 	}
 	usegzip := viper.GetBool("proxy.usegzip")
 	for _, route := range routes {
-		// indiviually proxy each request
+		// individually proxy each request
 		proxy := proxy.Tunnel(route)
 		if usegzip != true {
 			go router.HandleFunc(route.Path, func(w http.ResponseWriter, r *http.Request) {
@@ -76,5 +75,7 @@ func main() {
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("ListenAndServe error: %v", err)
 	}
+
+
 
 }
